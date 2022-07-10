fb6abf8ce9d0e854f3564727446fafe6e3b32314
Artyom Petrov <artyom.petrov@gmail.com>
2022-07-10 11:05:26 +0300

    App component with CatalogScreen page is ready, task points from 1 to 10

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,21 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import CatalogScreen from '../../pages/catalog-screen/catalog-screen';
+import Film from '../../types/film';
+import User from '../../types/user';
+
+type AppScreenProps = {
+  film: Film;
+  user: User;
+  filmCardsCount: number;
+}
+
+function App({film, user, filmCardsCount,}: AppScreenProps): JSX.Element {
+  return (
+    <CatalogScreen
+      film = {film}
+      user = {user}
+      filmCardsCount={filmCardsCount}
+    />
+  );
 }
 
 export default App;
